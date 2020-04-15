3a59c135b4861ded8952521024c81093cb570836
Nadav Bar Lev <nadav.barlev@moovit.com>
2020-04-16 00:52:03 +0300

    Init xcode project

---
+++ NBCommonKit/NBCommonKit/NBCommonKit.h
@@ -0,0 +1,19 @@
+//
+//  NBCommonKit.h
+//  NBCommonKit
+//
+//  Created by Nadav Bar Lev on 16/04/2020.
+//  Copyright © 2020 Nadav Bar Lev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NBCommonKit.
+FOUNDATION_EXPORT double NBCommonKitVersionNumber;
+
+//! Project version string for NBCommonKit.
+FOUNDATION_EXPORT const unsigned char NBCommonKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NBCommonKit/PublicHeader.h>
+
+
